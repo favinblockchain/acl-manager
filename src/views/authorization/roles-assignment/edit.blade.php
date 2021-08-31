93bdd4e777e41ed1c733478eb385a503e77d4715
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-08-31 11:20:31 +0430

    Fixed permissions list bug in user permissions edit section

--- src/views/authorization/roles-assignment/edit.blade.php
+++ src/views/authorization/roles-assignment/edit.blade.php
@@ -57,28 +57,26 @@
                                             @endforeach
                                         </div>
                                     </div>
-                                    @if(auth()->user()->is_admin && auth()->user()->hasRole(['programmer']))
-                                        @if ($permissions)
-                                            <p class="mb-2"><strong>دسترسی‌ها</strong></p>
-                                            <div class="mb-4">
-                                                <div class="row">
-                                                    @foreach ($permissions as $permission)
-                                                        <div class="col-md-3 mb-2">
-                                                            <div class="custom-control custom-checkbox">
-                                                                <input type="checkbox"
-                                                                       name="permissions[]"
-                                                                       value="{{$permission->id}}"
-                                                                       {!! $permission->assigned ? 'checked' : '' !!}
-                                                                       class="custom-control-input"
-                                                                       id="{{ 'permission'.$permission->id  }}">
-                                                                <label class="custom-control-label" for="{{ 'permission'.$permission->id  }}">{{$permission->display_name}}</label>
-                                                                {{--                                                    <div class="invalid-feedback">Example invalid feedback text</div>--}}
-                                                            </div>
+                                    @if ($permissions)
+                                        <p class="mb-2"><strong>دسترسی‌ها</strong></p>
+                                        <div class="mb-4">
+                                            <div class="row">
+                                                @foreach ($permissions as $permission)
+                                                    <div class="col-md-3 mb-2">
+                                                        <div class="custom-control custom-checkbox">
+                                                            <input type="checkbox"
+                                                                   name="permissions[]"
+                                                                   value="{{$permission->id}}"
+                                                                   {!! $permission->assigned ? 'checked' : '' !!}
+                                                                   class="custom-control-input"
+                                                                   id="{{ 'permission'.$permission->id  }}">
+                                                            <label class="custom-control-label" for="{{ 'permission'.$permission->id  }}">{{$permission->display_name}}</label>
+                                                            {{--                                                    <div class="invalid-feedback">Example invalid feedback text</div>--}}
                                                         </div>
-                                                    @endforeach
-                                                </div>
+                                                    </div>
+                                                @endforeach
                                             </div>
-                                        @endif
+                                        </div>
                                     @endif
                                     <div>
                                         <button class="btn btn-success" type="submit">ثبت</button>
