b2bc09fe318d9aba08bd6068c607b48d0c10c53f
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-10-27 12:28:39 +0330

    Added display_name to RolesController create method, fixed empty permission names bug

--- src/Http/Controllers/Authorization/RolesController.php
+++ src/Http/Controllers/Authorization/RolesController.php
@@ -42,7 +42,7 @@ class RolesController
     {
         return View::make('vendor.AclManager.authorization.edit', [
             'model' => null,
-            'permissions' => $this->permissionModel::all(['id', 'name']),
+            'permissions' => $this->permissionModel::all(['id', 'name', 'display_name']),
             'type' => 'role',
         ]);
     }
